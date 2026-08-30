94bf05858e3b650392ec8db0fdd18dc99ade772d
followmepls <pimmaneenimitraporn@gmail.com>
2026-08-30 17:48:32 +0700

    Refactor registration and response handling

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,71 +7,83 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required" });
-    }
-
-    if (isValid(username)) {
-        return res.status(409).json({ message: "User already exists!" });
-    }
-
-    users.push({ username, password });
-    return res.status(200).json({
-        message: "User successfully registered. Now you can login"
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username and password are required"
     });
+  }
+
+  if (isValid(username)) {
+    return res.status(409).json({
+      message: "User already exists!"
+    });
+  }
+
+  users.push({
+    username,
+    password
+  });
+
+  return res.status(200).json({
+    message: "User successfully registered. Now you can login"
+  });
 });
 
 public_users.get('/', (req, res) => {
-    return res.status(200).json(books);
+  return res.status(200).json(books);
 });
 
 public_users.get('/isbn/:isbn', (req, res) => {
-    const book = books[req.params.isbn];
+  const book = books[req.params.isbn];
 
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-    }
+  if (!book) {
+    return res.status(404).json({
+      message: "Book not found"
+    });
+  }
 
-    return res.status(200).json(book);
+  return res.status(200).json(book);
 });
 
 public_users.get('/author/:author', (req, res) => {
-    const author = req.params.author;
-    const result = {};
+  const author = req.params.author;
+  const result = {};
 
-    Object.keys(books).forEach((isbn) => {
-        if (books[isbn].author === author) {
-            result[isbn] = books[isbn];
-        }
-    });
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].author === author) {
+      result[isbn] = books[isbn];
+    }
+  });
 
-    return res.status(200).json(result);
+  return res.status(200).json(result);
 });
 
 public_users.get('/title/:title', (req, res) => {
-    const title = req.params.title;
-    const result = {};
+  const title = req.params.title;
+  const result = {};
 
-    Object.keys(books).forEach((isbn) => {
-        if (books[isbn].title === title) {
-            result[isbn] = books[isbn];
-        }
-    });
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title === title) {
+      result[isbn] = books[isbn];
+    }
+  });
 
-    return res.status(200).json(result);
+  return res.status(200).json(result);
 });
 
 public_users.get('/review/:isbn', (req, res) => {
-    const book = books[req.params.isbn];
+  const book = books[req.params.isbn];
 
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-    }
+  if (!book) {
+    return res.status(404).json({
+      message: "Book not found"
+    });
+  }
 
-    return res.status(200).json(book.reviews);
+  return res.status(200).json(book.reviews);
 });
 
 module.exports.general = public_users;
